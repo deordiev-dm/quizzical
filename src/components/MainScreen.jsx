fe2f4bec563139000915bb4e9c180320e3d25123
deordiev-dm <dimadeordiev2002@gmail.com>
2024-08-09 17:49:59 +0300

    added ability to check new answers and start a new game

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,39 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Question from "./Question";
 import { nanoid } from "nanoid";
 import { decode } from "html-entities";
+import Confetti from "react-confetti";
 
 /*
  * // // todo: redesign allQuestion state
- *     // // todo: give an ID to all option when creating state
- *     // // todo: give an ID to a question when creating state
- *     // // todo: complete the chooseAnswer function
- * // --> todo: write the logic for checking correct answers
- * // todo: write the logic for starting a new game
- * // todo:imporove styles
- *     // // todo: give .selected class to the selected option
- *     // todo: create a smaller container
- *     // todo: make a question underline span the whole width of a container
- * // todo: start making a call to the API when the page first loads, not when MainScreen is being rendered
+ *         // // todo: give an ID to all option when creating state
+ *         // // todo: give an ID to a question when creating state
+ *         // // todo: complete the chooseAnswer function
+ * // // todo: write the logic for checking correct answers:
+ *         // // todo: count the number of correct answers
+ *         // // todo: check if an answer is selected for every question
+ *         // // todo: make sure user can't select anything after answers were checked
+ * // // todo: write the logic for starting a new game
+ * // todo: imporove styles:
+ *         // // todo: give .selected class to the selected option
+ *         // todo: make a question underline span the whole width of a container
+ *         // todo: create a smaller container
+ * // todo: make a call to an API when the page first loads
+ *         // ? I'm not sure if it's not a memory leak. Try it.
  */
 
 function MainScreen() {
   const [allQuestions, setAllQuestions] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+  const hasFetchedData = useRef(false);
 
   // pull questions data from openTriviaDB
-  useEffect(() => {
-    fetch(
-      "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
-    )
-      .then(res => res.json())
-      .then(data => setAllQuestions(data.results?.map(constructQuestionObj)));
-  }, []);
+  useEffect(fetchNewQuestions, [isChecked]);
+
+  function fetchNewQuestions() {
+    // if ref wasn't updated it will not fetch new data
+    if (!hasFetchedData.current) {
+      hasFetchedData.current = true;
+
+      console.log("fetching new data");
+
+      fetch(
+        "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
+      )
+        .then(res => res.json())
+        .then(data => setAllQuestions(data.results?.map(constructQuestionObj)));
+    }
+  }
 
   // helper functions
   function constructQuestionObj(item) {
-    const answers = [...item.incorrect_answers, item.correct_answer].map(
-      answer => ({
-        id: nanoid(),
+    const correctAnswerId = nanoid();
+
+    const answers = [item.correct_answer, ...item.incorrect_answers].map(
+      (answer, index) => ({
+        // give correct answer on the first place predefined id
+        id: index === 0 ? correctAnswerId : nanoid(),
         value: decode(answer),
       })
     );
@@ -42,10 +61,8 @@ function MainScreen() {
       id: nanoid(),
       question: item.question,
       answers: shuffleArr(answers),
-      correctAnswer: item.correct_answer,
-      selectedAnswerId: "",
-      // ! stopped here. come up with a way of checking correct answers
-      isSelectedCorrect: undefined,
+      correctAnswerId,
+      selectedAnswerId: undefined,
     };
   }
 
@@ -70,28 +87,64 @@ function MainScreen() {
     });
   }
 
+  function countCorrectAnswers() {
+    return allQuestions.reduce((correctAnswers, question) => {
+      if (question.selectedAnswerId === question.correctAnswerId) {
+        return correctAnswers + 1;
+      } else {
+        return correctAnswers;
+      }
+    }, 0);
+  }
+
+  function handleClick() {
+    setIsChecked(prevState => {
+      if (prevState) {
+        hasFetchedData.current = false;
+      }
+
+      return !prevState;
+    });
+  }
+
   // construct JSX elements
   let questionComponents = [];
 
   if (allQuestions) {
-    questionComponents = allQuestions.map(item => {
+    questionComponents = allQuestions.map(question => {
       return (
         <Question
-          key={item.id}
-          questionId={item.id}
-          question={decode(item.question)}
-          answers={item.answers}
-          selectedAnswerId={item.selectedAnswerId}
+          key={question.id}
+          questionId={question.id}
+          question={decode(question.question)}
+          answers={question.answers}
+          selectedAnswerId={question.selectedAnswerId}
+          correctAnswerId={question.correctAnswerId}
           chooseAnswer={chooseAnswer}
+          isChecked={isChecked}
         />
       );
     });
   }
 
+  console.log(isChecked);
+
   return (
     <section className="quiz">
+      {isChecked && countCorrectAnswers() === 5 && <Confetti />}
+
       {questionComponents}
-      <button className="quiz__button button">Check answers</button>
+      <footer className="quiz__footer">
+        <button
+          className="quiz__button button"
+          onClick={handleClick}
+        >
+          {isChecked ? "Play again" : "Check answers"}
+        </button>
+        {isChecked && (
+          <p className="quiz__result">{`You scored ${countCorrectAnswers()}/5 correct answers`}</p>
+        )}
+      </footer>
     </section>
   );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,21 +1,47 @@
-function Question(props) {
-  const buttons = props.answers.map(answer => {
-    return (
-      <button
-        key={answer.id}
-        className={`question__option ${
-          props.selectedAnswerId === answer.id ? "selected" : ""
-        }`}
-        onClick={() => props.chooseAnswer(props.questionId, answer.id)}
-      >
-        {answer.value}
-      </button>
-    );
-  });
+function Question({
+  questionId,
+  question,
+  answers,
+  selectedAnswerId,
+  correctAnswerId,
+  chooseAnswer,
+  isChecked,
+}) {
+  function getButtonClass(answerId) {
+    if (!isChecked) {
+      return answerId === selectedAnswerId
+        ? "question__option selected"
+        : "question__option";
+    }
+
+    // in case answers were already checked
+    if (answerId === correctAnswerId)
+      return "question__option selected-correct";
+    // if selected isn't correct
+    if (answerId === selectedAnswerId) return "question__option selected-wrong";
+
+    // otherwise
+    return "question__option wrong";
+  }
+
+  const buttons = answers.map(answer => (
+    <button
+      key={answer.id}
+      className={getButtonClass(answer.id)}
+      onClick={() => {
+        if (!isChecked) {
+          chooseAnswer(questionId, answer.id);
+        }
+      }}
+      disabled={isChecked}
+    >
+      {answer.value}
+    </button>
+  ));
 
   return (
     <article className="question">
-      <h2 className="question__title title">{props.question}</h2>
+      <h2 className="question__title title">{question}</h2>
       <div className="question__options">{buttons}</div>
     </article>
   );
